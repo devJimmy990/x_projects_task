1277e6ba525b9dd00f056bd563fdc696ec2f723f
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-05-14 20:41:09 +0300

    feat: setup basic theme style

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,46 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:x_projects_task/core/constants/colors_manager.dart';
+import 'package:x_projects_task/core/theme/text_style.dart';
+
+class AppThemeData {
+  static final ThemeData light = ThemeData(
+    primaryColor: ColorsManager.blackSecondary,
+    scaffoldBackgroundColor: ColorsManager.white,
+    colorScheme: const ColorScheme.light(
+      brightness: Brightness.light,
+      primary: ColorsManager.blackSecondary,
+      // onPrimary: ColorsManager.white,
+      // secondary: ColorsManager.secondary,
+      onSecondary: ColorsManager.black,
+      // surface: ColorManager.bgLight,
+      // onSurface: ColorManager.textPrimaryLight,
+      // error: ColorManager.error,
+      // onError: ColorManager.white,
+    ),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      surfaceTintColor: ColorsManager.white,
+      backgroundColor: ColorsManager.white,
+      foregroundColor: ColorsManager.white,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: ColorsManager.blackPrimary,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.dark,
+      ),
+    ),
+    // tabBarTheme: TabBarTheme(
+    //   labelColor: ColorManager.textPrimaryLight,
+    //   unselectedLabelColor: ColorManager.grey,
+    //   indicator: UnderlineTabIndicator(
+    //     borderSide: BorderSide(color: ColorManager.lightPrimary, width: 2.0),
+    //   ),
+    // ),
+    // floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    //   backgroundColor: ColorManager.actionButton,
+    //   foregroundColor: ColorManager.white,
+    // ),
+    textTheme: AppTextStyle.theme,
+    // inputDecorationTheme: MyInputFieldTheme.light,
+  );
+}
